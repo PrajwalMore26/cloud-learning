56fa874b1b88ae4a8aad9972107def5713e5f5f0
Prajwal More <more.prajwal56@gmail.com>
2025-03-03 18:03:36 +0530

    stop tracking lib folder

--- lib/gcp.ts
+++
@@ -1,9 +0,0 @@
-import { Storage } from '@google-cloud/storage';
-
-// Initialize GCP client (auto-detects credentials from .env)
-export const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-});
-
-export const bucket = storage.bucket('praj-image-learn');
