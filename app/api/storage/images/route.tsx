d41a85d918e4981539dcb0cff9d5c3b1706ccacf
Prajwal More <more.prajwal56@gmail.com>
2025-03-07 12:28:12 +0530

    logs deleted and full-screen viewing issue solved

--- app/api/storage/images/route.tsx
+++ app/api/storage/images/route.tsx
@@ -1,53 +1,6 @@
 import { NextResponse } from "next/server";
 import { bucket } from "@/lib/gcp";
 
-// export async function GET() {
-//     try {
-//         const [files] = await bucket.getFiles();
-//         console.log(
-//             "Fetched files:",
-//             files.map((file) => file.name)
-//         );
-
-//         if (!files.length) {
-//             console.log("No files found in bucket");
-//             return NextResponse.json({ message: "No files found in bucket" });
-//         }
-
-//         const urls = await Promise.all(
-//             files.map(async (file) => {
-//                 try {
-//                     const [url] = await file.getSignedUrl({
-//                         action: "read",
-//                         expires: Date.now() + 15 * 60 * 1000, // 15 minutes
-//                     });
-//                     if (!url || typeof url !== "string") {
-//                         console.warn(`Invalid URL for file: ${file.name}`);
-//                         return null;
-//                     }
-//                     //console.log(`Signed URL for ${file.name}:`, url);
-//                     return url;
-//                 } catch (err) {
-//                     console.error(
-//                         `Failed to get signed URL for ${file.name}:`,
-//                         err
-//                     );
-//                     return null;
-//                 }
-//             })
-//         );
-
-//         // Filter out null values in case some URLs failed to generate
-//         return NextResponse.json(urls.filter(Boolean));
-//     } catch (err) {
-//         console.error("Error fetching images", err);
-//         return NextResponse.json(
-//             { error: "Failed to fetch images" },
-//             { status: 500 }
-//         );
-//     }
-// }
-
 export async function GET() {
     try {
         const [files] = await bucket.getFiles();
@@ -60,7 +13,7 @@ export async function GET() {
         const urls = await Promise.all(
             files.map(async (file) => {
                 try {
-                    console.log("Generating signed URL for file:", file.name);
+                    // console.log("Generating signed URL for file:", file.name);
                     const [url] = await file.getSignedUrl({
                         action: "read",
                         expires: Date.now() + 15 * 60 * 1000, // 15 minutes
